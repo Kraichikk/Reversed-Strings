6cf9b01c71fa6908a15594718c27682006e29cb2
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2023-02-24 10:50:57 +0200

    Add project files.

---
+++ Reversed Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Reversed_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static string Solution(string str)
+        {
+            char[] cArr = str.ToCharArray();
+            string temp = "";
+
+            for (int i = cArr.Length - 1; i > -1; i--)
+            {
+                temp += cArr[i];
+            }
+            return temp;
+        }
+    }
+}
